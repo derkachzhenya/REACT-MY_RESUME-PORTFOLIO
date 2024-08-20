c70bbb34f6a24d147c0976b60025a3b53e1f016a
Yevhen <zheka4573@gmail.com>
2024-08-21 02:07:01 +0300

    Framer Motion

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,39 +5,84 @@ import { FaVuejs } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaBootstrap } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    },
 
-
+})
 
 const Technologies = () => {
     return (
         <div className="border-b border-neutral-800 pb-24">
-            <h1 className="my-20 text-center text-4xl">Technologies</h1>
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.h1
+            whileInView={{ opacity: 1, y: 0}}
+            initial={{ opacity: 0, y: -100}}
+            transition={{ duration: 1.5}}
+            className="my-20 text-center text-4xl">Technologies</motion.h1>
+            <motion.div whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1.5 }}
+                className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaPhp className="text-7xl text-blue-700" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(3)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaLaravel className="text-7xl text-red-700" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <IoLogoJavascript className="text-7xl text-white-800" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaVuejs className="text-7xl text-green-700" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(6)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <RiTailwindCssFill className="text-7xl text-cyan-400" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(4)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaBootstrap className="text-7xl text-violet-600" />
-                </div>
-                <div className="rounded-2xl border-4 border-neutral-800 p-4">
+                </motion.div>
+                <motion.div
+                    variants={iconVariants(2)}
+                    initial="initial"
+                    animate="animate"
+                    className="rounded-2xl border-4 border-neutral-800 p-4">
                     <FaGithub className="text-7xl text-white" />
-                </div>
-
-
-            </div>
+                </motion.div>
+            </motion.div>
         </div>
     );
 };
