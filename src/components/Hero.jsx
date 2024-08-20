409cbdd60ca6b15464cb6d9e6eaf90c9f35a4e9a
Yevhen <zheka4573@gmail.com>
2024-08-21 00:17:10 +0300

    contact section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
                     <div className="flex flex-col items-center lg:items-start">
                         <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Yevhen Derkach</h1>
                         <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500
-                        bg-clip-text text-4xl tracking-tight text-transparent">Full Stack PHP Developer</span>
+                        bg-clip-text text-3xl tracking-tight text-transparent">Full Stack PHP Developer</span>
                         <p className="my-2 max-w-xl py-6 font-light tracking-tight">{HERO_CONTENT}</p>
                     </div>
                 </div>
