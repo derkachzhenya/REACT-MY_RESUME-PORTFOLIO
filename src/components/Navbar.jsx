72caa81433846b0a5983235a4bcd98ece20a439d
Yevhen <zheka4573@gmail.com>
2024-08-21 14:47:26 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/YevhenDerkachLogo.png";
-import { FaLinkedinIn, FaGithub, FaFacebook, FaInstagram } from "react-icons/fa6";
+import {  FaGithub, FaFacebook, FaInstagram } from "react-icons/fa6";
 const Navbar = () => {
     return (
         <nav className=" mb-20 flex items-center justify-between py-6">
@@ -7,9 +7,8 @@ const Navbar = () => {
                 <img className="mx-2 w-10" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <a href="https://www.linkedin.com/in/yevhen-derkach-9b805b182/" target="_blank" rel="noopener noreferrer">
-                    <FaLinkedinIn />
-                </a>
+                
+              
                 <a href="https://github.com/derkachzhenya" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                 </a>
