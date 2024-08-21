3a0fcd38855f3fc606fee7628a43c8f39cefcd84
Yevhen <zheka4573@gmail.com>
2024-08-21 10:21:12 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/YevhenDerkachLogo.png";
-import { FaLinkedinIn, FaGithub, FaSquareXTwitter, FaInstagram } from "react-icons/fa6";
+import { FaLinkedinIn, FaGithub, FaFacebook, FaInstagram } from "react-icons/fa6";
 const Navbar = () => {
     return (
         <nav className=" mb-20 flex items-center justify-between py-6">
@@ -7,16 +7,16 @@ const Navbar = () => {
                 <img className="mx-2 w-10" src={logo} alt="logo" />
             </div>
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <a href="www.linkedin.com/in/yevhen-derkach-9b805b182" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/yevhen-derkach-9b805b182/" target="_blank" rel="noopener noreferrer">
                     <FaLinkedinIn />
                 </a>
-                <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/derkachzhenya" target="_blank" rel="noopener noreferrer">
                     <FaGithub />
                 </a>
-                <a href="https://twitter.com/your-twitter-username" target="_blank" rel="noopener noreferrer">
-                    <FaSquareXTwitter />
+                <a href="https://www.facebook.com/zhenya.derkach/" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook />
                 </a>
-                <a href="https://www.instagram.com/your-instagram-username" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.instagram.com/derkach_yevhen/" target="_blank" rel="noopener noreferrer">
                     <FaInstagram />
                 </a>
             </div>
